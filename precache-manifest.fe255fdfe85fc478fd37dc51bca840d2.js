5fb9241256b791c17ab5a696f4a176d7dc1ee149
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-10-05 14:55:40 +0530

    Updates

---
+++ precache-manifest.fe255fdfe85fc478fd37dc51bca840d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30db872b740200806bed023e253385e1",
+    "url": "/react-routing/index.html"
+  },
+  {
+    "revision": "d92398455355eeb5beca",
+    "url": "/react-routing/static/css/main.c09e2229.chunk.css"
+  },
+  {
+    "revision": "8bc881412e2a4461e442",
+    "url": "/react-routing/static/js/2.0c4db7fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-routing/static/js/2.0c4db7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92398455355eeb5beca",
+    "url": "/react-routing/static/js/main.c5c859a8.chunk.js"
+  },
+  {
+    "revision": "febc0792d0b8238670a9",
+    "url": "/react-routing/static/js/runtime-main.1d86498f.js"
+  }
+]);
