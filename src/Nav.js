f724b6fdbef61bc9e93995245722b9f9cdd1c268
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-10-05 13:41:46 +0530

    route implemented

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,24 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Nav() {
+  const NavStyle = {
+    color: "white",
+    textDecoration: "none",
+  };
+
   return (
     <div>
       <nav>
-        <h3>Logo</h3>
+        <Link to="/" style={NavStyle}>
+          <h3>Logo</h3>
+        </Link>
         <ul className="nav-links">
-          <li>About</li>
-          <li>Shop</li>
+          <Link to="/about" style={NavStyle}>
+            <li>About</li>
+          </Link>
+          <Link to="/shop" style={NavStyle}>
+            <li>Shop</li>
+          </Link>
         </ul>
       </nav>
     </div>
